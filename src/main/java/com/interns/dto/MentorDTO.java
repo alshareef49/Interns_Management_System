ed5023fb8768cfb82578edb4efeddb8bb571f576
alshareef49 <alshareef4039@gmail.com>
2023-03-03 11:14:33 +0530

    creating data transfer object

---
+++ src/main/java/com/interns/dto/MentorDTO.java
@@ -0,0 +1,37 @@
+package com.interns.dto;
+
+public class MentorDTO {
+    private Integer mentorId;
+    private String mentorName;
+    private Integer numberOfProjectMentored;
+
+    public MentorDTO(Integer mentorId, String mentorName, Integer numberOfProjectMentored) {
+        this.mentorId = mentorId;
+        this.mentorName = mentorName;
+        this.numberOfProjectMentored = numberOfProjectMentored;
+    }
+
+    public Integer getMentorId() {
+        return mentorId;
+    }
+
+    public void setMentorId(Integer mentorId) {
+        this.mentorId = mentorId;
+    }
+
+    public String getMentorName() {
+        return mentorName;
+    }
+
+    public void setMentorName(String mentorName) {
+        this.mentorName = mentorName;
+    }
+
+    public Integer getNumberOfProjectMentored() {
+        return numberOfProjectMentored;
+    }
+
+    public void setNumberOfProjectMentored(Integer numberOfProjectMentored) {
+        this.numberOfProjectMentored = numberOfProjectMentored;
+    }
+}
---
+++ src/main/java/com/interns/dto/projectDTO.java
@@ -0,0 +1,50 @@
+package com.interns.dto;
+
+import java.time.LocalDate;
+
+public class projectDTO {
+    private Integer projectId;
+    private String projectName;
+    private LocalDate releaseDate;
+
+    private MentorDTO mentorDTO;
+
+    public projectDTO(Integer projectId, String projectName, LocalDate releaseDate, MentorDTO mentorDTO) {
+        this.projectId = projectId;
+        this.projectName = projectName;
+        this.releaseDate = releaseDate;
+        this.mentorDTO = mentorDTO;
+    }
+
+    public Integer getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(Integer projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public MentorDTO getMentorDTO() {
+        return mentorDTO;
+    }
+
+    public void setMentorDTO(MentorDTO mentorDTO) {
+        this.mentorDTO = mentorDTO;
+    }
+}
